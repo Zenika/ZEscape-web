2c575562e7dcf6ad6a7b6535d6a3bbfc1659b0a6
audreygentili <audrey.gentili@zenika.com>
2023-08-07 14:28:04 +0200

    chore: fix lint issue

--- src/modules/adventures/components/AdventureList.component.tsx
+++ src/modules/adventures/components/AdventureList.component.tsx
@@ -1,7 +1,12 @@
-type Props = {};
+import { useEffect } from "react";
+import { fetchAdventures } from "../api";
+
+type Props = { id: number };
 
 export const AdventureListComponent: React.FC<Props> = () => {
-  // TODO fetch list in useEffect
+  useEffect(() => {
+    fetchAdventures().then()
+  }, [])
 
   return <ul></ul>;
 };
